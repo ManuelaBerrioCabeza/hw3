20a43c2d59726d607df73ca28f0f87a0a7bf1639
Manuela Berrio <berrio@usc.edu>
2025-03-08 03:29:46 +0000

    done 2

--- heap.h
+++ heap.h
@@ -15,7 +15,7 @@ public:
    *          as an argument and returns a bool if the first argument has
    *          priority over the second.
    */
-  Heap(int m=2, PComparator c = PComparator());
+  Heap(int m=2, PComparator c = PComparator()): m_(m), comp_(c){};
 
   /**
   * @brief Destroy the Heap object
@@ -61,15 +61,26 @@ public:
 
 private:
   /// Add whatever helper functions and data members you need below
-
-
-
+  int m_;
+  std::vector<T> heap_;
+  PComparator comp_;
+  void heapifyDown(size_t i);
+  void heapifyUp(size_t i);
 
 };
 
 // Add implementation of member functions here
 
+//push
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::push(const T& item)
+{
+  heap_.push_back(item);
+  heapifyUp(heap_.size()-1);
 
+}
+
+//top
 // We will start top() for you to handle the case of 
 // calling top on an empty heap
 template <typename T, typename PComparator>
@@ -81,17 +92,15 @@ T const & Heap<T,PComparator>::top() const
     // ================================
     // throw the appropriate exception
     // ================================
-
-
+    throw std::underflow_error("Heap is empty");
   }
   // If we get here we know the heap has at least 1 item
   // Add code to return the top element
-
-
-
+  return heap_.front();
 }
 
 
+//pop
 // We will start pop() for you to handle the case of 
 // calling top on an empty heap
 template <typename T, typename PComparator>
@@ -101,11 +110,67 @@ void Heap<T,PComparator>::pop()
     // ================================
     // throw the appropriate exception
     // ================================
+    throw std::underflow_error("Heap is empty");
+  }
+  std::swap(heap_.front(), heap_.back());
+  heap_.pop_back();
+  heapifyDown(0);
+}
 
+//empty
+template <typename T, typename PComparator>
+bool Heap<T,PComparator>::empty() const
+{
+  return heap_.empty();
 
+}
+
+//size
+template <typename T, typename PComparator>
+size_t Heap<T,PComparator>::size() const
+{
+  return heap_.size();
+}
+
+template <typename T, typename PComparator>
+Heap<T, PComparator>::~Heap() {
+    heap_.clear();
+}
+
+//heapifyUp
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::heapifyUp(size_t i)
+{
+  if (i == 0){
+    return; } 
+
+  size_t parentIndex = (i - 1) / m_;
+
+  if (comp_(heap_[i], heap_[parentIndex])) {
+    std::swap(heap_[i], heap_[parentIndex]);
+    heapifyUp(parentIndex);  
   }
 
+}
 
+//heapifyDown
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::heapifyDown(size_t i)
+{
+  size_t bestIndex = i;  
+    
+    for (int j = 1; j <= m_; ++j) {
+        size_t childIndex = m_ * i + j;
+        if (childIndex < heap_.size() && comp_(heap_[childIndex], heap_[bestIndex])) {
+            bestIndex = childIndex;  
+        }
+    }
+
+    
+    if (bestIndex != i) {
+        std::swap(heap_[i], heap_[bestIndex]);
+        heapifyDown(bestIndex);
+    }
 
 }
 
--- llrec.cpp
+++ llrec.cpp
@@ -4,4 +4,23 @@
 // Provide your implementation of llpivot below
 //*********************************************
 
+void llpivot(Node*& head, Node*& smaller, Node*& larger, int pivot){
+  if(!head){
+    smaller = NULL;
+    larger = NULL;
+    return;
+  }
 
+  Node* nextNode = head-> next;
+  if (head -> val <=pivot){
+    head-> next = smaller;
+    smaller = head;
+  }
+  else{
+    head-> next = larger;
+    larger = head;
+  }
+
+  head = nextNode;
+  llpivot(head,smaller,larger,pivot);
+}
